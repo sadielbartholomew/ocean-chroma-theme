e0330f9e910f3f868b64dfa1b50a1a0e6551c574
Sadie Louise Bartholomew <sadie.bartholomew@ncas.ac.uk>
2021-03-01 01:13:05 +0000

    Minor modifications to basic theme plus tidy and improve organisation

--- ocean-chroma-theme.el
+++ ocean-chroma-theme.el
@@ -1,5 +1,5 @@
-;;; ocean-chroma-theme.el :: Fresh dark colour theme for Emacs 24+
-;;
+;;; ocean-chroma-theme.el ::: Fresh dark colour theme for Emacs 24+
+
 ;; A fresh dark colour theme for Emacs 24+ based on blue-green and
 ;; primary colours.
 ;;
@@ -18,7 +18,11 @@
 ;;     Author: Sadie L. Bartholomew (sadielbartholomew)
 ;;     Maintainer: Sadie L. Bartholomew (sadielbartholomew)
 ;;     URL: https://github.com/sadielbartholomew/ocean-chroma-theme
-;;     Max line lengthof file: 99
+;;     Maximum line length of file: 99
+
+;; Tip: to view all faces and therefore preview those set by this theme, run:
+;;   M-x customize-face
+;; with the cursor not on any text, then select 'y' to see all faces.
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -28,33 +32,38 @@
 
 (let*
 
-    ;;; Define the full (fairly minimal) colour scheme used by the theme
-    (
-     ;; Shades of 'ocean' i.e. blue-green colours, increasingly dark
-     (duck-egg           "#8FBCB5")     ; i.e. very-light-teal
-     (light-teal         "#33859D")
-     (mid-teal           "#004B66")
-     (dark-teal          "#00384D")
-     (very-dark-teal     "#001B25")
+  ;;; Define the full (fairly minimal) colour scheme used by the theme
+  (
+   ;; Shades of 'ocean' i.e. blue-green colours, increasingly dark
+   (duck-egg           "#8FBCB5")  ; i.e. very-light-teal
+   (light-teal         "#33859D")
+   (mid-teal           "#004B66")
+   (dark-teal          "#00384D")
+   (very-dark-teal     "#001B25")
+   (extra-dark-teal    "#00131A")
 
-     ;; Complementary 'chroma' primary (RGB) colours (plus secondary yellow)
-     (chroma-red         "#D22D2D")
-     (chroma-blue        "#3980C6")
-     (chroma-yellow      "#FFDF80")
-     (chroma-green       "#00B386")
+   ;; Complementary 'chroma' primary (RGB) colours (plus secondary yellow)
+   (chroma-red         "#D22D2D")
+   (chroma-blue        "#3980C6")
+   (chroma-yellow      "#FFDF80")
+   (chroma-green       "#00B386")
 
-     ;; Darker variations on 'chroma' colours (minus dark yellow which is ugly)
-     (dark-chroma-red    "#9C1626")
-     (dark-chroma-blue   "#00264D")
-     (dark-chroma-green  "#004D39")
+   ;; Darker variations on some 'chroma' colours
+   (dark-chroma-red    "#9C1626")
+   (dark-chroma-blue   "#00264D")
 
-     ;; Variants on black and white
-     (off-black          "#0A0F14")
-     (off-white          "#E5E5FF")     ; a very bright-looking white
-     )
+   ;; Variants on black and white
+   (off-black          "#0A0F14")
+   (off-white          "#E5E5FF")  ; a very bright-looking white
 
-  ;;; Apply the above faces with various styling to define and set the theme.
+   ;; Some other complementary colours for extra elements
+   (maroon             "#28000E")
+  )
+
+  ;;; Apply the above faces with various styling to define and set the theme
   (custom-theme-set-faces `ocean-chroma
+
+                          ;; **Generic built-in elements**
 
                           ;; Default face
                           `(default ((t
@@ -73,12 +82,11 @@
                                                    :bold t
                                                    :underline nil
                                                    :slant normal))))
-                          `(error
-                            ((t
-                              (:foreground ,chroma-red
-                                           :bold t
-                                           :underline nil
-                                           :slant normal))))
+                          `(error ((t
+                                    (:foreground ,chroma-red
+                                                 :bold t
+                                                 :underline nil
+                                                 :slant normal))))
 
                           ;; Font lock faces
                           `(font-lock-builtin-face ((t
@@ -86,8 +94,17 @@
                           `(font-lock-comment-face ((t
                                                      (:foreground ,mid-teal
                                                                   :slant italic))))
+                          `(font-lock-comment-delimiter-face ((t
+                                                               (:inherit font-lock-comment-face))))
                           `(font-lock-constant-face ((t
                                                       (:foreground ,chroma-yellow))))
+                          ;; Otherwise I would set doc-face as :inherit font-lock-comment-face, but
+                          ;; I think documentation is inherently tied to the code (unlike comments
+                          ;; which are for developers) so should be highlighted as a code string,
+                          ;; though use italics like with comments to imply it is non-functional.
+                          `(font-lock-doc-face ((t
+                                                 (:inherit font-lock-string-face
+                                                           :slant italic))))
                           `(font-lock-function-name-face ((t
                                                            (:foreground ,light-teal))))
                           `(font-lock-keyword-face ((t
@@ -103,26 +120,10 @@
                                                   (:foreground ,light-teal))))
                           `(font-lock-variable-name-face ((t
                                                            (:foreground ,off-white))))
-
-                          ;; Inheriting font lock faces
-                          `(font-lock-comment-delimiter-face ((t
-                                                               (:inherit font-lock-comment-face))))
-                          ;; Otherwise I would set doc-face as :inherit font-lock-comment-face, but
-                          ;; I think documentation is inherently tied to the code (unlike comments
-                          ;; which are for developers) so should be highlighted as a code string.
-                          `(font-lock-doc-face ((t
-                                                 (:inherit font-lock-string-face
-                                                           :slant italic))))
                           `(font-lock-warning-face ((t
                                                      (:inherit warning))))
 
-
-                          ;; Cursor
-                          `(cursor ((t
-                                     (:foreground ,off-white
-                                                  :background ,off-black))))
-
-                          ;; Highlighting
+                          ;; Highlighting, including incremental search
                           `(highlight ((t
                                         (:foreground ,chroma-yellow
                                                      :background ,dark-chroma-red))))
@@ -130,7 +131,8 @@
                                              (:foreground ,dark-chroma-blue
                                                           :background ,off-white))))
                           `(region ((t
-                                     (:background ,dark-chroma-blue))))
+                                     (:background ,dark-teal
+                                                  :weight bold))))
                           `(match ((t
                                     (:inherit highlight))))
                           `(isearch ((t
@@ -138,7 +140,7 @@
                           `(isearch-fail ((t
                                            (:foreground ,chroma-red))))
 
-                          ;; Fringes and fringe elements
+                          ;; Fringe and fringe elements
                           `(fringe ((t
                                      (:inherit default
                                                :foreground ,light-teal
@@ -146,27 +148,59 @@
                           `(linum ((t
                                     (:inherit fringe
                                               :foreground ,mid-teal))))
+                          `(vertical-border ((t
+                                              (:foreground ,dark-teal))))
 
-                          ;; Elements at the foot of a window (or frame): minibuffer and mode line
+                          ;; Elements at the head and foot of a window (or frame)
+                          `(header-line ((t
+                                          (:background ,very-dark-teal
+                                                       :weight ultra-bold))))
                           `(minibuffer-prompt ((t
                                                 (:inherit highlight
                                                           :weight ultra-bold))))
+                          `(mode-line ((t
+                                        (:foreground ,chroma-blue
+                                                     :background ,dark-chroma-blue
+                                                     :weight ultra-bold))))
                           `(mode-line-inactive ((t
                                                  (:inherit fringe
                                                            :background ,dark-teal))))
-                          `(mode-line ((t
-                                        (:foreground ,off-white
-                                                     :background ,dark-chroma-blue
-                                                     :weight ultra-bold))))
+                          `(mode-line-buffer-id ((t
+                                                  (:foreground ,off-white))))
+                          `(mode-line-highlight ((t
+                                                  (:foreground ,chroma-yellow))))
+                          `(mode-line-emphasis ((t
+                                                 (:foreground ,chroma-red))))
 
-                          ;; Links in traditional hyperlink blues
+                          ;; Links and buttons: use traditional style of underlined blues
                           `(link ((t
                                    (:foreground ,chroma-blue
                                                 :underline t))))
                           `(link-visited ((t
                                            (:foreground ,dark-chroma-blue
                                                         :underline t))))
-)) ; let* ends here
+                          '(button ((t
+                                     (:inherit link
+                                               :weight bold))))
+
+                          ;; Other general elements
+                          `(cursor ((t
+                                     (:foreground ,off-white
+                                                  :background ,off-black))))
+                          `(tooltip ((t
+                                      (:foreground ,off-black
+                                                   :background ,duck-egg))))
+                          `(shadow ((t
+                                     (:foreground ,duck-egg))))
+                          `(escape-glyph ((t
+                                           (:foreground ,dark-chroma-red
+                                                        :bold t))))
+
+                          ;; Note: not including scrollbar or toolbar settings as these are usually
+                          ;; set by the OS or environment, hence themes don't usually set them.
+
+  )
+) ; let* ends here
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
